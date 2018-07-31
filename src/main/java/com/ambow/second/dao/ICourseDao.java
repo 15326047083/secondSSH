cfa73962388ebed542c0dc4723e3ca74eb41ff42
dell <15326047083@163.com>
2018-07-31 17:07:22 +0800

    wang

--- src/main/java/com/ambow/second/dao/ICourseDao.java
+++ src/main/java/com/ambow/second/dao/ICourseDao.java
@@ -2,5 +2,11 @@ package com.ambow.second.dao;
 
 import com.ambow.second.entity.Course;
 
+
+import java.util.List;
+
 public interface ICourseDao extends ICommonDao<Course> {
+   
+
+
 }
--- src/main/java/com/ambow/second/dao/impl/CourseDao.java
+++ src/main/java/com/ambow/second/dao/impl/CourseDao.java
@@ -2,8 +2,17 @@ package com.ambow.second.dao.impl;
 
 import com.ambow.second.dao.ICourseDao;
 import com.ambow.second.entity.Course;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CourseDao extends CommonDao<Course> implements ICourseDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+
+
 }

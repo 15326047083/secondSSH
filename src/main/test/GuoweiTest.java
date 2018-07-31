03a71d1aba532c4bec44e5c23ef34a572e7c35e5
Administrator <1532607083@163.com>
2018-07-31 20:44:02 +0800

    郭伟：测试类

---
+++ src/main/test/GuoweiTest.java
@@ -0,0 +1,41 @@
+import com.ambow.second.entity.User;
+import com.ambow.second.service.IUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * 读取spring配置文件
+ */
+@ContextConfiguration(locations = {"classpath:spring.xml", "classpath:spring-hibernate.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class GuoweiTest {
+
+    // 自动注入userService
+    @Autowired
+    private IUserService userService;
+
+    @Test
+    public void findUserById(){
+        User user = new User();
+        user.setId("4028abf064efab9f0164efaba1d80000");
+        System.out.println(userService.findUserById(user));
+    }
+    @Test
+    public void saveOrUpdate(){
+        User user=new User();
+        user.setId("4028abf064efab9f0164efaba1d80000");
+        user.setName("test");
+        System.out.println(userService.saveOrUpdate(user));
+    }
+    @Test
+    public void delete(){
+        User user=new User();
+        user.setId("3");
+        System.out.println(userService.delete(user));
+    }
+
+}
